258f7f143abb3e93af959c187000ba40410fe74a
Adrian Samarripas <asamarripas@gmail.com>
2018-05-06 06:37:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-
+var index = 0;
 
 function init() {
-  
-let index = 0;
 
-document.body.addEventListener('keydown', function(e){
+document.body.addEventListener('keydown',function(e){
   var key = parseInt(e.detail || e.which);
   
-   if (code[index] === key) {
-      index++;
-
-      if (index === code.length) {
-        alert('Hurray!');
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
+  if (key === code[index]){
+    index++;
+  }else{
+    index = 0;
+  }
+  
+  if (index === code.length){
+    alert("Wake me up... I can't wake up!");
+    index = 0;
+  }
 });
   
 }
