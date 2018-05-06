508817ec399d4c58f540f7e455d4596dfd8d51f6
Adrian Samarripas <asamarripas@gmail.com>
2018-05-06 06:25:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-const input = document.body.querySelector('input');
 
 function init() {
 
 let index = 0;
 
-input.addEventListener('keydown',function(e){
+document.body.addEventListener('keydown',function(e){
   const key = parseInt(e.detail || e.which);
 });
   if (key === code[index]){
